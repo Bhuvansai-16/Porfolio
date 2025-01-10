9014c770d72ca71eac0ff29b93ed682fb31ca908
Bhuvansai-16 <chbhuvansai522@gmail.com>
2025-01-10 21:23:54 +0530

    con

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,12 +5,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> 5a7a55546c51f4b79b7ce2f7aff24278c8aca90d
